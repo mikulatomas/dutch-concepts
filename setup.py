b3bf50c8ce61eccf0c1ee53b9fa93fa709660514
Tomáš Mikula <mail@tomasmikula.cz>
2021-07-01 10:36:59 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="dutch-concepts",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://github.com/mikulatomas/dutch-concepts",
+    author="Tomáš Mikula",
+    author_email="mail@tomasmikula.cz",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
