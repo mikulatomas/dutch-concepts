65e29324e436d452993fe14fe91c4aea40aabafd
Tomáš Mikula <mail@tomasmikula.cz>
2021-07-13 21:14:22 +0200

    Rename and hide unnecessary attributes

--- dutch_concepts/dutch_concepts.py
+++ dutch_concepts/dutch_concepts.py
@@ -10,45 +10,45 @@ URL = "https://github.com/mikulatomas/dutch-concepts/raw/master/data/dutch_data.
 
 
 class DutchConcepts:
-    def __init__(self, root, language="en"):
-        self.root = os.path.abspath(root)
+    def __init__(self, download_dir, language="en"):
+        self.download_dir = os.path.abspath(download_dir)
 
         if language not in ["en", "nl"]:
             raise ValueError("Wrong language only 'en' and 'nl' is supported.")
 
         self.language = language
-        self.dataset_dir = os.path.join(self.root, "dutch_data", language)
-        self.zip_file = URL.rsplit("/", 1)[-1]
+        dataset_dir = os.path.join(self.download_dir, "dutch_data", language)
+        zip_file = URL.rsplit("/", 1)[-1]
         
-        if not os.path.exists(self.dataset_dir) or not os.listdir(self.dataset_dir):
+        if not os.path.exists(dataset_dir) or not os.listdir(dataset_dir):
             logging.info("Downloading dataset.")
-            self.__download()
+            self.__download(zip_file)
             logging.info("Downloading is done.")
             logging.info("Extracting dataset.")
-            self.__extract()
+            self.__extract(zip_file)
             logging.info("Extracting done.")
         else:
             logging.info("Dataset directory is not empty, skipping download.")
 
-        self.category_features = load_features(self.dataset_dir, FeatureType.CATEGORY)
+        self.category_features = load_features(dataset_dir, FeatureType.CATEGORY)
 
-        self.exemplar_features = load_features(self.dataset_dir, FeatureType.EXEMPLAR)
+        self.exemplar_features = load_features(dataset_dir, FeatureType.EXEMPLAR)
 
-        self.exemplar_judgements = load_exemplar_judgements(self.dataset_dir)
+        self.exemplar_judgements = load_exemplar_judgements(dataset_dir)
 
-        self.exemplar_similarities = load_exemplar_similarity(self.dataset_dir)
+        self.exemplar_similarities = load_exemplar_similarity(dataset_dir)
     
     def __repr__(self):
-        return f"DutchConcepts({self.root}, {self.language})"
+        return f"DutchConcepts({self.download_dir}, {self.language})"
 
-    def __download(self):
+    def __download(self, zip_file):
         with urllib.request.urlopen(URL) as f:
-            with open(os.path.join(self.root, self.zip_file), "wb") as out_f:
+            with open(os.path.join(self.download_dir, zip_file), "wb") as out_f:
                 out_f.write(f.read())
 
-    def __extract(self):
-        with zipfile.ZipFile(os.path.join(self.root, self.zip_file), "r") as zip_f:
-            zip_f.extractall(self.root)
+    def __extract(self, zip_file):
+        with zipfile.ZipFile(os.path.join(self.download_dir, zip_file), "r") as zip_f:
+            zip_f.extractall(self.download_dir)
 
         logging.info("Removing downloaded ZIP file.")
-        os.remove(os.path.join(self.root, self.zip_file))
+        os.remove(os.path.join(self.download_dir, zip_file))
